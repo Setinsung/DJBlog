0e2a1f8df66f9277606d86a80e6a467408691cf7
Setinsung <1968989251@qq.com>
2023-04-12 21:29:05 +0800

    登录jwt

--- blog_server/app/service/admin.js
+++ blog_server/app/service/admin.js
@@ -5,7 +5,7 @@ const Service = require('egg').Service;
 
 class AdminService extends Service {
   async adminLogin(params) {
-    const { ctx } = this;
+    const { ctx, app } = this;
     const resUser = await ctx.model.Admin.findOne({
       username: params.userName,
     });
@@ -14,13 +14,28 @@ class AdminService extends Service {
         msg: '用户名或密码错误',
       };
     }
+    // 匹配密码
     const isMatch = await ctx.helper.comparePassword(params.password, resUser.password);
     if (!isMatch) {
       return {
         msg: '用户名或密码错误',
       };
     }
-    console.log(isMatch);
+    // 成功登录后，生成token
+    const token = app.jwt.sign({ ...resUser }, app.config.jwt.secret, { expiresIn: '1h' });
+    // 直接放到cookie中，httpOnly为true表示只有服务器端可以获取cookie
+    ctx.cookies.set('token', token, {
+      maxAge: 86400000,
+      httpOnly: true,
+    });
+    // 返回token和username
+    return {
+      data: {
+        token,
+        userName: resUser.username,
+      },
+      msg: '登录成功',
+    };
   }
 }
 
